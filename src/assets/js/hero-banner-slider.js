2d81dbc7f0b6457380277f3b47bd8e1ff25f83d4
wqsharif <waqas461.designer@gmail.com>
2021-05-19 07:26:26 +0400

    minor changes

--- src/assets/js/hero-banner-slider.js
+++ src/assets/js/hero-banner-slider.js
@@ -5,24 +5,16 @@ heroBanner.css('height', windowHeight);
 heroBanner.find('.banner-img .cover img').css('height', windowHeight);
 
 
-var sliderSwiper = new Swiper('.hero-banner.with-slider .swiper-container', {
-    slidesPerView: 1,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  }); 
-
 
   var sliderAuto = new Swiper('.slider-auto .hero-banner.with-slider .swiper-container', {
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
     centeredSlides: true,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
